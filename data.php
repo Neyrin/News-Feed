48d71c3813498aebe8277c0db1659a7ddeb933db
Isa Areschoug <42944657+Neyrin@users.noreply.github.com>
2018-10-21 14:57:20 +0200

    Create data.php

---
+++ data.php
@@ -0,0 +1,94 @@
+<?php
+
+
+$authors = [
+   12345 => 'Malcolm Reynolds',
+   23451 => 'Hoban Washburne',
+   34512 => 'Kaylee Frye',
+   45123 => 'Jayne Cobb',
+   51234 => 'Zoë Washburne',
+];
+
+$articles = [
+   [
+    //Mal
+   'title' => 'Captain\'s log',
+   'content' => 'Shiny!', //TODO länka int txt fil
+   'date' => '2016-06-12',
+   'likes'=> 15, //TODO om tid finns, gör denna responsiv
+   'authorID' => '12345',
+   ],
+   [
+    //Wash
+   'title' => 'Curse your sudden but inevitable betrayal!',
+   'content' => 'Shiny!', //TODO länka int txt fil
+   'date' => '2016-12-14',
+   'likes'=> 15, //TODO om tid finns, gör denna responsiv
+   'authorID' => '23451',
+   ],
+   [
+    //Kaylee
+   'title' => 'Strawberry Pie',
+   'content' => 'Shiny!', //TODO länka int txt fil
+   'date' => '2017-10-30',
+   'likes'=> 15, //TODO om tid finns, gör denna responsiv
+   'authorID' => '34512',
+   ],
+   [
+    //Jayne
+   'title' => 'Why my hat is awesome!',
+   'content' => 'Shiny!', //TODO länka int txt fil
+   'date' => '2017-09-05',
+   'likes'=> 15, //TODO om tid finns, gör denna responsiv
+   'authorID' => '45123',
+   ],
+   [
+    //Zoë
+   'title' => 'I\'m surrounded by idiots',
+   'content' => 'Shiny!', //TODO länka int txt fil
+   'date' => '2016-07-26',
+   'likes'=> 15, //TODO om tid finns, gör denna responsiv
+   'authorID' => '51234',
+   ],
+   [
+    //Mal
+   'title' => 'Captain\'s log nr.2',
+   'content' => 'Shiny!', //TODO länka int txt fil
+   'date' => '2017-06-21',
+   'likes'=> 15, //TODO om tid finns, gör denna responsiv
+   'authorID' => '12345',
+
+   ],
+   [
+    //Kaylee
+   'title' => 'Why all the unecessary parts?',
+   'content' => 'Shiny!', //TODO länka int txt fil
+   'date' => '2016-09-23',
+   'likes'=> 15, //TODO om tid finns, gör denna responsiv
+   'authorID' => '34512',
+   ],
+   [
+    //Wash
+   'title' => 'I\'m a leaf on the wind',
+   'content' => 'Shiny!', //TODO länka int txt fil
+   'date' => '2016-08-14',
+   'likes'=> 15, //TODO om tid finns, gör denna responsiv
+   'authorID' => '23451',
+   ],
+   [
+    //Zoë
+   'title' => 'Nice place for an ambush',
+   'content' => 'Shiny!', //TODO länka int txt fil
+   'date' => '2017-07-12',
+   'likes'=> 15, //TODO om tid finns, gör denna responsiv
+   'authorID' => '51234',
+   ],
+   [
+    //Jayne
+   'title' => 'Meet Vera',
+   'content' => 'Shiny!', //TODO länka int txt fil
+   'date' => '2017-02-18',
+   'likes'=> 15, //TODO om tid finns, gör denna responsiv
+   'authorID' => '45123',
+   ],
+];

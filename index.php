c6633114ca0dcd718f2b37cbbd6cbaf215dc6743
Isa Areschoug <42944657+Neyrin@users.noreply.github.com>
2018-10-31 12:14:16 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,45 +3,54 @@ declare(strict_types=1);
 require __DIR__.'/data.php';
 require __DIR__.'/functions.php';
 
-usort($articles, "sortFunction");
+usort($articles, "sortFunctionDate");
 
 ?>
-
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
    <head>
       <meta charset="utf-8">
-      <title>FakeNews</title>
+      <title>Fake News</title>
+      <link rel='stylesheet' href='fakenews.css'>
+      <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
    </head>
    <body>
-      <header></header>
+      <header class='siteHeader'>
+         <h1>Fake News</h1>
+      </header> <!--siteHeader-->
+
       <div class='newsFeed'>
 <!-- Foreach-loop to assign all values from $articles array to variables-->
          <?php
          foreach ($articles as $article):
-             $contentTitle = $article['title'];
-             $contentText = $article['content'];
-             $contentLikes = $article['likes'];
-             $contentDate = $article['date'];
-             $contentAuthor = $authors[$article['authorID']]; //Gets name from $authors array using the ID listed in $articles
+             $title = $article['title'];
+             $text = $article['content'];
+             $likes = $article['likes'];
+             $date = $article['date'];
+             $author = $authors[$article['authorID']]; //Gets name from $authors array using the ID listed in $articles
          ?>
 
              <div class='article'>
                <header class='articleHeader'>
-                  <h3 class='contentTitle'><?= $contentTitle; ?></h3>
-               </header>
+                  <h3 class='title'><?= $title; ?></h3>
+               </header> <!--/articleHeader-->
 
                <div class='articleContent'>
-                  <p class='contentText'><?= $contentText; ?></p>
-               </div>
+                  <p class='text'><?= $text; ?></p>
+               </div> <!--articleContent-->
 
-               <footer class='articleFooter'>
-                  <p class='contentAuthor'><?= $contentAuthor; ?></p>
-                  <p class='contentDate'><?= $contentDate; ?></p>
-                  <p class='contentLikes'><?= $contentLikes; ?></p>
-               </footer>
-            </div> <!--article-->
+               <div class='articleInfo'>
+                  <p class='author'><?='<strong>Written by:</strong> ' . $author; ?></p>
+                  <p class='date'><?='<strong>Published:</strong> ' . $date; ?></p>
+                  <p class='likes'><?='<strong>Likes:</strong> ' . $likes; ?></p>
+               </div> <!--/articleInfo-->
+
+            </div> <!--/article-->
          <?php endforeach; ?>
-      </div> <!--newsFeed-->
+      </div> <!--/newsFeed-->
+
+      <button onclick="toTopFunction()" id="topBtn" title="Go to top">^</button>
+
+      <script src='script.js'></script>
    </body>
 </html>
